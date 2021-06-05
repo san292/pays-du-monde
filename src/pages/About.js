05aba72aeb8436ef62b17adb3bf942e4d6fae967
san man <sance1921@gmail.com>
2021-06-06 01:35:28 +0200

    initiation projet pays du monde

---
+++ src/pages/About.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+    <Navigation />
+    <Logo />
+      <h1>à propos </h1>
+      <br />
+      <p>lorem ipsum Duis ex minim est qui occaecat incididunt voluptate labore
+        aliquip sit tempor eu fugiat. Eu exercitation ipsum laboris ad eu quis
+        cupidatat. Do consequat aute ad enim tempor minim aute. Sint quis
+        excepteur ipsum dolor occaecat consequat non incididunt et qui qui
+        cupidatat aliquip. Ad dolor commodo aliquip quis officia sit laboris
+        nisi eu ipsum exercitation duis ut.
+      </p>
+      <br />
+      <p>Elit consectetur et et velit esse Lorem irure exercitation. Laboris ex
+        deserunt nostrud excepteur enim incididunt dolore cillum. In labore
+        cillum ex consectetur nulla enim Lorem anim. Fugiat laborum amet
+        proident ipsum anim tempor elit. Commodo quis sunt aliqua eu dolore
+        occaecat sit minim sit minim ut. Eu cupidatat duis sit nostrud
+        incididunt dolore incididunt qui aute non proident esse adipisicing.
+        Voluptate occaecat veniam enim et ut proident laboris id esse. 
+      </p>
+    </div>
+  );
+};
+
+export default About;
